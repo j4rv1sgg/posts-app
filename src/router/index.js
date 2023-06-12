3e73db11f3ad68911f3ab5b3fda0a1b150f74dce
j4rv1sgg <jrvs.inc@gmail.com>
2023-06-12 10:48:17 +0200

    fixed routes

--- src/router/index.js
+++ src/router/index.js
@@ -4,11 +4,14 @@ import Posts from "../pages/Posts";
 import Login from "../pages/Login";
 
 
+
 export const privateRoutes = [
     {path: '/about', component: <About />},
     {path: '/posts', component: <Posts />},
     {path: '/posts/:id', component: <PostIdPage />},
 ]
 export const publicRoutes = [
-    {path: '/login', component: <Login/>}
+    {path: '/login', component: <Login/>},
+    {path: '/', component: <Login/>},
+    {path: '*', component: <Login/>}
 ]
